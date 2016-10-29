390aa55724818e02e0110c22123251e45ab2a85b
mrbian <554565808@qq.com>
2016-10-29 21:53:42 +0800

    加入启动shell

---
+++ start.sh
@@ -0,0 +1,9 @@
+export PATH="/usr/lib/lightdm/lightdm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/mysql/bin:/opt/android-sdk-linux/tools:/opt/android-sdk-linux/platform-tools:~/usr/lib/jvm/jdk-6/bin"
+
+dir="$(dirname $0)"
+
+env=${dir}/.env/bin/activate
+source ${env}
+
+entry_file="${dir}/index.py"
+python ${entry_file}

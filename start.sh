06b5f7086b5629ea770ea487e20532d008c98161
mrbian <554565808@qq.com>
2016-10-31 21:42:09 +0800

    bash 和 sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export PATH="/usr/lib/lightdm/lightdm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/mysql/bin:/opt/android-sdk-linux/tools:/opt/android-sdk-linux/platform-tools:~/usr/lib/jvm/jdk-6/bin"
 
 dir="$(dirname $0)"
